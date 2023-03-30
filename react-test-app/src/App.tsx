39da71a36a0ff30ebbe8b6da2c33c976a31b61dd
Jissmon Jose <37608527+jisshub@users.noreply.github.com>
2023-03-30 18:47:12 +0530

    user state

---
+++ react-test-app/src/App.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+// material UI
+import { Card } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+// axios
+import axios from 'axios';
+
+
+function App() {
+  const [userInfo, setUserInfo] = useState([])
+
+  async function fetchUserInfo(){
+    axios.get('https://randomuser.me/api')
+      .then(res => setUserInfo(res.data.results))
+  } 
+
+  function refreshUser() {
+    fetchUserInfo()
+  }
+
+  useEffect(() => {
+    fetchUserInfo()
+  }, [])
+  
+  return (
+    <div className="App">    
+    {userInfo.map((user) => (
+      <Card sx={{ minWidth: 345 }}>
+        <CardContent>
+          <Typography variant="h4" component="div">
+            {user?.name?.first} {user?.name?.last}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {user?.email}
+          </Typography>
+        </CardContent>
+        <CardActions className='card-action'>
+          <Button 
+            variant="text" 
+            className="btn"
+            onClick={refreshUser}
+            >
+              Refresh
+          </Button>
+        </CardActions>
+      </Card>
+    ))}
+    </div>
+  )
+}
+
+export default App
